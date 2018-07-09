4504c4f8e496454b6ff773fd1da65a139fae16c7
Cédric Belin <cedric@belin.io>
2018-07-09 18:41:39 +0200

    Added an example code

---
+++ example/main.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+use FreeMobile\{Client, ClientException};
+
+/**
+ * Sends an SMS notification.
+ */
+function main(): void {
+  try {
+    $client = new Client('your account identifier', 'your API key');
+    // For example: new Client('12345678', 'a9BkVohJun4MAf')
+
+    $client->sendMessage('Hello World!');
+    echo 'The message was sent successfully.';
+  }
+
+  catch (\Throwable $e) {
+    echo 'An error occurred: ', $e->getMessage(), PHP_EOL;
+    if ($e instanceof ClientException) echo 'From: ', $e->getUri(), PHP_EOL;
+  }
+}
