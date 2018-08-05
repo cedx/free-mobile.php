2f5b5cfdf319c884ec8027d74c874e8d535a680a
Cédric Belin <cedric@belin.io>
2018-08-05 03:13:07 +0200

    Dropped the dependency on PHPUnit-Expect

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 namespace FreeMobile;
 
-use function PHPUnit\Expect\{expect, fail, it};
 use PHPUnit\Framework\{TestCase};
 use Psr\Http\Message\{UriInterface};
 
@@ -15,65 +14,60 @@ class ClientTest extends TestCase {
    * @test Client::__construct
    */
   public function testConstructor(): void {
-    it('should throw an exception if the username or password is empty', function() {
-      expect(function() { new Client('', ''); })->to->throw(\InvalidArgumentException::class);
-    });
+    // It should throw an exception if the username or password is empty.
+    $this->expectException(\InvalidArgumentException::class);
+    new Client('', '');
   }
 
   /**
    * @test Client::getEndPoint
    */
   public function testGetEndPoint(): void {
-    it('should not be empty by default', function() {
-      $endPoint = (new Client('anonymous', 'secret'))->getEndPoint();
-      expect($endPoint)->to->be->an->instanceOf(UriInterface::class);
-      expect((string) $endPoint)->to->equal('https://smsapi.free-mobile.fr');
-    });
+    // It should not be empty by default.
+    $endPoint = (new Client('anonymous', 'secret'))->getEndPoint();
+    assertThat($endPoint, isInstanceOf(UriInterface::class));
+    assertThat((string) $endPoint, equalTo('https://smsapi.free-mobile.fr'));
 
-    it('should be an instance of the `Uri` class', function() {
-      $endPoint = (new Client('anonymous', 'secret', 'http://localhost'))->getEndPoint();
-      expect($endPoint)->to->be->an->instanceOf(UriInterface::class);
-      expect((string) $endPoint)->to->equal('http://localhost');
-    });
+    // It should be an instance of the `Uri` class.
+    $endPoint = (new Client('anonymous', 'secret', 'http://localhost'))->getEndPoint();
+    assertThat($endPoint, isInstanceOf(UriInterface::class));
+    assertThat((string) $endPoint, equalTo('http://localhost'));
   }
 
   /**
    * @test Client::sendMessage
    */
   public function testSendMessage(): void {
-    it('should not send invalid messages with valid credentials', function() {
-      try {
-        (new Client('anonymous', 'secret'))->sendMessage('');
-        fail('An empty message with valid credentials should not be sent');
-      }
+    // It should not send invalid messages with valid credentials.
+    try {
+      (new Client('anonymous', 'secret'))->sendMessage('');
+      $this->fail('An empty message with valid credentials should not be sent');
+    }
 
-      catch (\Throwable $e) {
-        expect($e)->to->be->an->instanceOf(\InvalidArgumentException::class);
-      }
-    });
+    catch (\Throwable $e) {
+      assertThat($e, isInstanceOf(\InvalidArgumentException::class));
+    }
 
-    it('should throw a `ClientException` if a network error occurred', function() {
-      try {
-        (new Client('anonymous', 'secret', 'http://localhost'))->sendMessage('Hello World!');
-        fail('A message with an invalid endpoint should not be sent');
-      }
+    // It should throw a `ClientException` if a network error occurred.
+    try {
+      (new Client('anonymous', 'secret', 'http://localhost'))->sendMessage('Hello World!');
+      $this->fail('A message with an invalid endpoint should not be sent');
+    }
 
-      catch (\Throwable $e) {
-        expect($e)->to->be->an->instanceOf(ClientException::class);
-      }
-    });
+    catch (\Throwable $e) {
+      assertThat($e, isInstanceOf(ClientException::class));
+    }
 
+    // It should send valid messages with valid credentials.
     if (is_string($username = getenv('FREEMOBILE_USERNAME')) && is_string($password = getenv('FREEMOBILE_PASSWORD'))) {
-      it('should send valid messages with valid credentials', function() use ($password, $username) {
-        try {
-          (new Client($username, $password))->sendMessage('Bonjour Cédric !');
-          expect(true)->to->be->true;
-        }
+      try {
+        (new Client($username, $password))->sendMessage('Bonjour Cédric !');
+        assertThat(true, isTrue());
+      }
 
-        catch (\Throwable $e) {
-          expect($e)->to->be->an->instanceOf(ClientException::class);
-        }
-      });
+      catch (\Throwable $e) {
+        assertThat($e, isInstanceOf(ClientException::class));
+      }
     }
   }
 }
