44e6c00312266e82e3a66a0c1a72ae49e6b81c77
Cédric Belin <cedric@belin.io>
2019-07-11 17:59:45 +0200

    Ported the tests to BDD assertions

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 namespace FreeMobile;
 
+use function PHPUnit\Expect\{expect, it};
 use GuzzleHttp\Psr7\{Uri};
 use PHPUnit\Framework\{TestCase};
 
@@ -9,43 +10,25 @@ class ClientTest extends TestCase {
 
   /** @test Client->__construct() */
   function testConstructor(): void {
-    // It should throw an exception if the username or password is empty.
-    $this->expectException(\InvalidArgumentException::class);
-    new Client('', '');
+    it('should throw an exception if the username or password is empty', function() {
+      expect(function() { new Client('', ''); })->to->throw(\InvalidArgumentException::class);
+    });
   }
 
   /** @test Client->sendMessage() */
   function testSendMessage(): void {
-    // It should not send invalid messages with valid credentials.
-    try {
-      (new Client('anonymous', 'secret'))->sendMessage('');
-      $this->fail('Exception not thrown.');
-    }
+    it('should not send invalid messages with valid credentials', function() {
+      expect(function() { (new Client('anonymous', 'secret'))->sendMessage(''); })->to->throw(\InvalidArgumentException::class);
+    });
 
-    catch (\Throwable $e) {
-      assertThat($e, isInstanceOf(\InvalidArgumentException::class));
-    }
+    it('should throw a `ClientException` if a network error occurred', function() {
+      expect(function() { (new Client('anonymous', 'secret', new Uri('http://localhost/')))->sendMessage('Hello World!'); })->to->throw(ClientException::class);
+    });
 
-    // It should throw a `ClientException` if a network error occurred.
-    try {
-      (new Client('anonymous', 'secret', new Uri('http://localhost/')))->sendMessage('Hello World!');
-      $this->fail('Exception not thrown.');
-    }
-
-    catch (\Throwable $e) {
-      assertThat($e, isInstanceOf(ClientException::class));
-    }
-
-    // It should send valid messages with valid credentials.
-    try {
+    it('should send valid messages with valid credentials', function() {
       $username = (string) getenv('FREEMOBILE_USERNAME');
       $password = (string) getenv('FREEMOBILE_PASSWORD');
-      (new Client($username, $password))->sendMessage('Bonjour Cédric, à partir de PHP !');
-      assertThat(true, isTrue());
-    }
-
-    catch (\Throwable $e) {
-      $this->fail($e->getMessage());
-    }
+      expect(function() use ($username, $password) { (new Client($username, $password))->sendMessage('Bonjour Cédric, à partir de PHP !'); })->to->not->throw;
+    });
   }
 }
