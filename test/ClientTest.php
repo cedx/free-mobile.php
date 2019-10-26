5fefb40bfa745de36810b0561d17d262c7c6789e
Cédric Belin <cedric@belin.io>
2019-10-26 15:52:38 +0200

    Using arrow functions

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -11,24 +11,24 @@ class ClientTest extends TestCase {
   /** @testdox constructor */
   function testConstructor(): void {
     it('should throw an exception if the username or password is empty', function() {
-      expect(function() { new Client('', ''); })->to->throw(\InvalidArgumentException::class);
+      expect(fn() => new Client('', ''))->to->throw(\InvalidArgumentException::class);
     });
   }
 
   /** @testdox ->sendMessage() */
   function testSendMessage(): void {
     it('should not send invalid messages with valid credentials', function() {
-      expect(function() { (new Client('anonymous', 'secret'))->sendMessage(''); })->to->throw(\InvalidArgumentException::class);
+      expect(fn() => (new Client('anonymous', 'secret'))->sendMessage(''))->to->throw(\InvalidArgumentException::class);
     });
 
     it('should throw a `ClientException` if a network error occurred', function() {
-      expect(function() { (new Client('anonymous', 'secret', new Uri('http://localhost/')))->sendMessage('Hello World!'); })->to->throw(ClientException::class);
+      expect(fn() => (new Client('anonymous', 'secret', new Uri('http://localhost/')))->sendMessage('Hello World!'))->to->throw(ClientException::class);
     });
 
     it('should send valid messages with valid credentials', function() {
       $username = (string) getenv('FREEMOBILE_USERNAME');
       $password = (string) getenv('FREEMOBILE_PASSWORD');
-      expect(function() use ($username, $password) { (new Client($username, $password))->sendMessage('Bonjour Cédric, à partir de PHP !'); })->to->not->throw;
+      expect(fn() => (new Client($username, $password))->sendMessage('Bonjour Cédric, à partir de PHP !'))->to->not->throw;
     });
   }
 }
