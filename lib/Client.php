1394dc6c85d888dae211c75ad7fe5ab00bb526ef
Cédric Belin <cedric@belin.io>
2020-03-25 15:38:58 +0100

    Using PSR-18 for the HTTP client

--- lib/Client.php
+++ lib/Client.php
@@ -1,11 +1,9 @@
 <?php declare(strict_types=1);
 namespace FreeMobile;
 
-use function GuzzleHttp\Psr7\{build_query};
-use GuzzleHttp\{Client as HTTPClient};
-use GuzzleHttp\Psr7\{Request, Uri, UriResolver};
 use League\Event\{Emitter};
 use Psr\Http\Message\{UriInterface};
+use Symfony\Component\HttpClient\{Psr18Client};
 
 /** Sends messages by SMS to a Free Mobile account. */
 class Client extends Emitter {
@@ -18,6 +16,9 @@ class Client extends Emitter {
 
   /** @var UriInterface The URL of the API end point. */
   private UriInterface $endPoint;
+
+  /** @var Psr18Client The HTTP client. */
+  private Psr18Client $http;
 
   /** @var string The identification key associated to the account. */
   private string $password;
@@ -34,9 +35,11 @@ class Client extends Emitter {
   function __construct(string $username, string $password, ?UriInterface $endPoint = null) {
     assert(mb_strlen($username) > 0);
     assert(mb_strlen($password) > 0);
-    $this->username = $username;
+
+    $this->http = new Psr18Client;
+    $this->endPoint = $endPoint ?? $this->http->createUri('https://smsapi.free-mobile.fr/');
     $this->password = $password;
-    $this->endPoint = $endPoint ?? new Uri('https://smsapi.free-mobile.fr/');
+    $this->username = $username;
   }
 
   /**
@@ -71,17 +74,17 @@ class Client extends Emitter {
   function sendMessage(string $text): void {
     assert(mb_strlen($text) > 0);
 
-    $uri = UriResolver::resolve($this->getEndPoint(), new Uri('sendmsg'))->withQuery(build_query([
+    $uri = $this->http->createUri("{$this->getEndPoint()}/sendmsg")->withQuery(http_build_query([
       'msg' => mb_substr(trim($text), 0, 160),
       'pass' => $this->getPassword(),
       'user' => $this->getUsername()
-    ]));
+    ], PHP_QUERY_RFC3986));
 
     try {
-      $request = new Request('GET', $uri);
+      $request = $this->http->createRequest('GET', $uri);
       $this->emit(new RequestEvent($request));
 
-      $response = (new HTTPClient)->send($request);
+      $response = $this->http->sendRequest($request);
       $this->emit(new ResponseEvent($response, $request));
     }
 
