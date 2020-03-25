d58691bf22aeec5024b0e97da16806e5bf36ba49
Cédric Belin <cedric@belin.io>
2020-03-25 12:42:32 +0100

    Replaced PHPUnit-Expect by plain PHPUnit for testing

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -1,23 +1,35 @@
 <?php declare(strict_types=1);
 namespace FreeMobile;
 
-use function PHPUnit\Expect\{expect, it};
 use GuzzleHttp\Psr7\{Uri};
-use PHPUnit\Framework\{TestCase};
+use PHPUnit\Framework\{Assert, TestCase};
+use function PHPUnit\Framework\{assertThat, isInstanceOf, isNull};
 
 /** @testdox FreeMobile\Client */
 class ClientTest extends TestCase {
 
   /** @testdox ->sendMessage() */
   function testSendMessage(): void {
-    it('should throw a `ClientException` if a network error occurred', function() {
-      expect(fn() => (new Client('anonymous', 'secret', new Uri('http://localhost/')))->sendMessage('Hello World!'))->to->throw(ClientException::class);
-    });
+    // It should throw a `ClientException` if a network error occurred.
+    try {
+      (new Client('anonymous', 'secret', new Uri('http://localhost/')))->sendMessage('Hello World!');
+      Assert::fail('Exception not thrown');
+    }
 
-    it('should send SMS messages if credentials are valid', function() {
+    catch (\Throwable $e) {
+      assertThat($e, isInstanceOf(ClientException::class));
+    }
+
+    // It should send SMS messages if credentials are valid.
+    try {
       $username = (string) getenv('FREEMOBILE_USERNAME');
       $password = (string) getenv('FREEMOBILE_PASSWORD');
-      expect(fn() => (new Client($username, $password))->sendMessage('Bonjour Cédric, à partir de PHP !'))->to->not->throw;
-    });
+      (new Client($username, $password))->sendMessage('Bonjour Cédric, à partir de PHP !');
+      assertThat(null, isNull());
+    }
+
+    catch (\Throwable $e) {
+      Assert::fail($e->getMessage());
+    }
   }
 }
