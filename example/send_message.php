858908e6468d0a5badbc0fbc8fe2864f9e2b63f0
Cédric Belin <cedric@belin.io>
2022-05-04 22:35:42 +0200

    Code formatting [skip ci]

--- example/send_message.php
+++ example/send_message.php
@@ -8,8 +8,6 @@ use Psr\Http\Client\ClientExceptionInterface;
  */
 try {
 	$client = new Client(account: "your account identifier", apiKey: "your API key");
-	// For example: new Client("12345678", "a9BkVohJun4MAf")
-
 	$client->sendMessage("Hello World from PHP!");
 	print "The message was sent successfully.";
 }
