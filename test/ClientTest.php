d06edadf1c432542f5864ca979a94261b2588531
Cédric Belin <cedric@belin.io>
2022-05-04 18:08:32 +0200

    Fixed the CI

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -42,15 +42,7 @@ class ClientTest extends TestCase {
 	 * @testdox ->sendMessage(): it should send SMS messages if the credentials are valid.
 	 */
 	function testValidCredentials(): void {
-		try {
 		$client = new Client(getenv("FREEMOBILE_ACCOUNT") ?: "", getenv("FREEMOBILE_API_KEY") ?: "");
 		assertThat($client->sendMessage("Hello Cédric, from PHP!"), isNull());
-		}
-
-		catch (RequestExceptionInterface $e) {
-			var_dump(getenv("FREEMOBILE_ACCOUNT"), getenv("FREEMOBILE_API_KEY"));
-			print implode(PHP_EOL, [$e->getCode(), $e->getMessage(), (string) $e->getRequest()->getUri()]);
-			throw $e;
-		}
 	}
 }
