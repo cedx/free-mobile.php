d2220b974ec1ceabb61f093cf88d9a6c4d960b8b
Cédric Belin <cedric@belin.io>
2022-05-04 17:40:23 +0200

    Improved the tests

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -11,28 +11,37 @@ use function PHPUnit\Framework\{assertThat, isInstanceOf, isNull};
 class ClientTest extends TestCase {
 
 	/**
-	 * @testdox ->sendMessage()
+	 * @testdox ->sendMessage(): it should throw a `NetworkExceptionInterface` if a network error occurred.
 	 */
-	function testSendMessage(): void {
-		// It should throw a `NetworkExceptionInterface` if a network error occurred.
+	function testNetworkError(): void {
 		try {
 			(new Client(account: "anonymous", apiKey: "secret", baseUrl: "http://localhost:10000/"))->sendMessage("Hello World!");
 			Assert::fail("Exception not thrown.");
 		}
+
 		catch (\Throwable $e) {
 			assertThat($e, isInstanceOf(NetworkExceptionInterface::class));
 		}
+	}
 
-		// It should throw a `RequestExceptionInterface` if the credentials are invalid.
+	/**
+	 * @testdox ->sendMessage(): it should throw a `RequestExceptionInterface` if the credentials are invalid.
+	 */
+	function testInvalidCredentials(): void {
 		try {
 			(new Client(account: "anonymous", apiKey: "secret"))->sendMessage("Hello World!");
 			Assert::fail("Exception not thrown.");
 		}
+
 		catch (\Throwable $e) {
 			assertThat($e, isInstanceOf(RequestExceptionInterface::class));
 		}
+	}
 
-		// It should send SMS messages if credentials are valid.
+	/**
+	 * @testdox ->sendMessage(): it should send SMS messages if the credentials are valid.
+	 */
+	function testValidCredentials(): void {
 		$client = new Client(getenv("FREEMOBILE_ACCOUNT") ?: "", getenv("FREEMOBILE_API_KEY") ?: "");
 		assertThat($client->sendMessage("Hello Cédric, from PHP!"), isNull());
 	}
