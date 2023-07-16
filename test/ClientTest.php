a1846d186d47d7223a0c9de4bf28b84156e31712
Cédric Belin <cedric@belin.io>
2023-07-16 13:01:15 +0200

    Replace the test framework

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -3,7 +3,7 @@
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\TestDox;
 use Psr\Http\Client\ClientExceptionInterface;
-use function phpunit\expect\{expect, it};
+use function PHPUnit\Framework\{assertThat, isNull};
 
 /**
  * Tests the features of the {@see Client} class.
@@ -11,21 +11,23 @@ use function phpunit\expect\{expect, it};
 #[TestDox('freemobile\Client')]
 final class ClientTest extends TestCase {
 
-	#[TestDox("->sendMessage()")]
+	#[TestDox("->sendMessage(): should throw a `ClientExceptionInterface` if a network error occurred.")]
 	function testNetworkError(): void {
-		it("should reject if a network error occurred", function() {
-			$client = new Client(account: "anonymous", apiKey: "secret", baseUrl: "http://localhost:10000/");
-			expect(fn() => $client->sendMessage("Hello World!"))->to->throw(ClientExceptionInterface::class);
-		});
+		$this->expectException(ClientExceptionInterface::class);
+		$client = new Client("anonymous", "secret", baseUrl: "http://localhost:10000/");
+		$client->sendMessage("Hello World!");
+	}
 
-		it("should reject if the credentials are invalid", function() {
-			$client = new Client(account: "anonymous", apiKey: "secret");
-			expect(fn() => $client->sendMessage("Hello World!"))->to->throw(ClientExceptionInterface::class);
-		});
+	#[TestDox("->sendMessage(): should throw a `ClientExceptionInterface` if the credentials are invalid.")]
+	function testInvalidCredentials(): void {
+		$this->expectException(ClientExceptionInterface::class);
+		$client = new Client("anonymous", "secret");
+		$client->sendMessage("Hello World!");
+	}
 
-		it("should send SMS messages if the credentials are valid", function() {
-			$client = new Client(getenv("FREEMOBILE_ACCOUNT") ?: "", getenv("FREEMOBILE_API_KEY") ?: "");
-			expect(fn() => $client->sendMessage("Hello Cédric, from PHP!"))->to->not->throw;
-		});
+	#[TestDox("->sendMessage(): should send SMS messages if the credentials are valid.")]
+	function testValidCredentials(): void {
+		$client = new Client(getenv("FREEMOBILE_ACCOUNT") ?: "", getenv("FREEMOBILE_API_KEY") ?: "");
+		assertThat($client->sendMessage("Hello Cédric, from PHP!"), isNull()); // @phpstan-ignore-line
 	}
 }
