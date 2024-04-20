8c3acc1851a5c3be10fd676cfbb4dde44f8f5550
Cédric Belin <cedric@belin.io>
2024-04-20 21:46:54 +0200

    Code formatting [skip ci]

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -11,24 +11,21 @@ use function PHPUnit\Framework\{assertThat, isNull};
 #[TestDox("Client")]
 final class ClientTest extends TestCase {
 
-	#[Test]
-	#[TestDox("sendMessage(): should throw a `ClientExceptionInterface` if a network error occurred.")]
+	#[Test, TestDox("sendMessage(): should throw a `ClientExceptionInterface` if a network error occurred.")]
 	function networkError(): void {
 		$this->expectException(ClientExceptionInterface::class);
 		$client = new Client("anonymous", "secret", baseUrl: "http://localhost:10000/");
 		$client->sendMessage("Hello World!");
 	}
 
-	#[Test]
-	#[TestDox("sendMessage(): should throw a `ClientExceptionInterface` if the credentials are invalid.")]
+	#[Test, TestDox("sendMessage(): should throw a `ClientExceptionInterface` if the credentials are invalid.")]
 	function invalidCredentials(): void {
 		$this->expectException(ClientExceptionInterface::class);
 		$client = new Client("anonymous", "secret");
 		$client->sendMessage("Hello World!");
 	}
 
-	#[Test]
-	#[TestDox("sendMessage(): should send SMS messages if the credentials are valid.")]
+	#[Test, TestDox("sendMessage(): should send SMS messages if the credentials are valid.")]
 	function validCredentials(): void {
 		$client = new Client(getenv("FREEMOBILE_ACCOUNT") ?: "", getenv("FREEMOBILE_API_KEY") ?: "");
 		assertThat($client->sendMessage("Hello Cédric, from PHP!"), isNull()); // @phpstan-ignore-line
